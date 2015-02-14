4c45fb5adfbed7b6c62815cb6c53b0c3f57412f4
Matthew Messinger <mattmess1221@gmail.com>
2015-02-13 21:15:10 -0500

    Getting repo ready

---
+++ src/main/java/mnm/mods/calligraphy/LiteModCalligraphy.java
@@ -0,0 +1,28 @@
+package mnm.mods.calligraphy;
+
+import java.io.File;
+
+import com.mumfrey.liteloader.LiteMod;
+
+public class LiteModCalligraphy implements LiteMod {
+
+    @Override
+    public String getName() {
+        return "Calligraphy";
+    }
+
+    @Override
+    public String getVersion() {
+        return "0.0";
+    }
+
+    @Override
+    public void init(File file) {
+
+    }
+
+    @Override
+    public void upgradeSettings(String version, File oldF, File newF) {
+
+    }
+}
